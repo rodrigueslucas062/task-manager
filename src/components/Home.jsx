3146b03f72e103172e20326352ec8df0a9b104b2
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-03-29 23:58:01 -0300

    ✨ Home To Do list

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+const { default: Navbar } = require("./Navbar")
+
+const Daily = () => {
+    return (
+        <div className="flex flex-col h-full items-center justify-center bg-zinc-900">
+            <Navbar />
+        </div>
+    )
+}
+
+export default Daily;
