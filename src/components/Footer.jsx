aa69142ae384fffe5634d13f98ab08c228b6e08f
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-03-30 18:35:35 -0300

    ✨🚧 Footer skeleton

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,19 @@
-const { Github } = require("lucide-react")
+import { Github } from "lucide-react"
 
 const Footer = () => {
     return (
-        <footer className="flex h-full items-center justify-between">
-            <a
-                className="flex select-none items-center gap-3 rounded-2xl px-4 py-2 transition-transform hover:ring-2 hover:ring-black active:scale-95 dark:hover:ring-white"
-                target="_blank"
-                href="https://github.com/rodrigueslucas062/task-manager"
-                rel="noreferrer"
-            >
-                <Github className="fill-black dark:fill-white" />
-                <span className="text-black dark:text-white">
-                    Repositório Github
-                </span>
-            </a>
+        <footer className="fixed bottom-0 h-16 w-full border-t-2 px-6 sm:flex">
+            <div className="flex items-center justify-around gap-4">
+                <a className="flex select-none items-center gap-3 rounded-2xl px-4 py-2 transition-transform hover:ring-2 active:scale-95 hover:ring-white"
+                    target="_blank"
+                    href="https://github.com/rodrigueslucas062/task-manager"
+                    rel="noreferrer" >
+                    <Github className="dark:fill-white" />
+                    <span className="dark:text-white">
+                        Repositório Github
+                    </span>
+                </a>
+            </div>
         </footer>
     )
 }
