a4f3a91116451812349867cfff14f10d3698b58c
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-04-01 00:21:14 -0300

    ✨ Navbar menus and layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import Image from "next/image"
 import Perfil from '../../public/images/perfil.jpeg'
-import { ChevronRight, FlaskConical, MoreHorizontal, Share2, X } from "lucide-react"
+import { ChevronRight, FlaskConical, Moon, MoreHorizontal, QrCode, Share2, Sun, Trash2, X } from "lucide-react"
 import { useEffect, useState } from "react"
+import { toast } from "sonner";
 
 const Navbar = () => {
     const [isMenuVisible, setIsMenuVisible] = useState(false);
     const [copied, setCopied] = useState(false)
+    const [isSun, setIsSun] = useState(true)
+
+    const handleToggleTheme = () => {
+        setIsSun(!isSun)
+    }
 
     useEffect(() => {
         const handleScroll = () => {
@@ -40,7 +46,7 @@ const Navbar = () => {
                 <Image className="rounded-xl border-2 border-zinc-900" src={Perfil} alt="Lucas Rodrigues" width={50} height={50} />
                 <h5 className="mb-1 text-xl font-semibold text-zinc-800">Lista diária</h5>
                 <Dialog.Root>
-                    <Dialog.Trigger className="visible bg-zinc-200 hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full">
+                    <Dialog.Trigger className="visible bg-zinc-200 hover:bg-zinc-400 text-zinc-900 hover:text-zinc-200 p-2 rounded-full">
                         <MoreHorizontal size={18} />
                     </Dialog.Trigger>
 
@@ -52,21 +58,40 @@ const Navbar = () => {
                                 </Dialog.Close>
                                 <div className="flex items-center justify-center gap-3 p-4">
                                     <div className="rounded-lg mt-8 inline-block w-5/6 relative text-zinc-900">
-                                        <div className="flex flex-col items-center justify-center">
-                                            <span className="font-semibold text-zinc-900 text-lg">Lucas Rodrigues</span>
-                                            <div className="flex mt-6 items-center justify-around gap-6">
-                                                <Share2 />
-                                                <span>Compartilhar esse projeto</span>
-                                                <div className="hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full" onClick={handleCopyLink}>
-                                                    <ChevronRight />
+                                        <div className="flex flex-col items-center justify-center space-y-2">
+                                            <span className="font-semibold text-zinc-900 text-lg">Lista diárias</span>
+                                            <div className="flex flex-col mt-6 space-y-4 font-semibold">
+                                                <div className="flex items-center justify-between px-3 rounded-md hover:bg-gray-300">
+                                                    <Share2 />
+                                                    <span>Compartilhar esse projeto</span>
+                                                    <div className="hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full" onClick={handleCopyLink}>
+                                                        <ChevronRight />
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </div>
-                                        <div className="relative flex justify-center items-center">
-                                            <div className="fixed flex bottom-0 mb-3">
-                                                <span className="text-sm text-zinc-900 font-semibold">Made for </span>
-                                                <FlaskConical className="text-lime-500" />{" "}
-                                                <span className="text-sm text-zinc-900 font-semibold">By Lucas</span>
+                                                <div
+                                                    className="flex items-center justify-between px-3 rounded-md hover:bg-gray-300"
+                                                    onClick={handleToggleTheme}
+                                                >
+                                                    {isSun ? <Sun /> : <Moon />}
+                                                    <span>Mudar tema</span>
+                                                    <div className="hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full">
+                                                        <ChevronRight />
+                                                    </div>
+                                                </div>
+                                                <div className="fixed flex flex-col space-y-2 bottom-0">
+                                                    <div className="flex items-center justify-between mb-2 px-3 rounded-md group hover:ring-2 hover:ring-red-600">
+                                                        <Trash2 className="w-6 h-6 group-hover:text-red-600" />
+                                                        <span className="flex-grow ml-4 group-hover:text-red-600">Remover todas as tarefas</span>
+                                                        <div className="hover:bg-zinc-300 hover:text-zinc-200 p-2 rounded-full group-hover:text-red-600">
+                                                            <ChevronRight className="w-6 h-6" />
+                                                        </div>
+                                                    </div>
+                                                    <div className="flex items-center justify-center pb-3">
+                                                        <span className="text-sm text-zinc-900 font-semibold">Made for </span>
+                                                        <FlaskConical className="text-lime-500" />{" "}
+                                                        <span className="text-sm text-zinc-900 font-semibold">By Lucas</span>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
