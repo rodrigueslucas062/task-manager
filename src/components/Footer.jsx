cf5e3f01689499076ef128ed291e303bb06766f0
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-04-01 23:59:37 -0300

    💄 Footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="fixed bottom-0 h-16 w-full border-t-2 px-6 sm:flex">
             <div className="flex items-center justify-around gap-4">
-                <a className="flex select-none items-center gap-3 rounded-2xl px-4 py-2 transition-transform hover:ring-2 active:scale-95 hover:ring-white"
+                <a className="flex select-none items-center gap-3 rounded-2xl px-4 py-2 transition-transform hover:ring-2 active:scale-95 hover:ring-white hover:backdrop-blur-sm hover:bg-gray-50/25"
                     target="_blank"
                     href="https://github.com/rodrigueslucas062/task-manager"
                     rel="noreferrer" >
