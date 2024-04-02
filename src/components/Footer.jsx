ca6463b900008e30f352046199f06754e92e57b1
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-04-02 01:16:01 -0300

    💄 Correction in the alignment of footer items

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Github } from "lucide-react"
 
 const Footer = () => {
     return (
-        <footer className="fixed bottom-0 h-16 w-full border-t-2 px-6 sm:flex">
+        <footer className="fixed bottom-0 h-16 w-full border-t-2 p-3 max-md:p-2 max-md:h-14">
             <div className="flex items-center justify-around gap-4">
                 <a className="flex select-none items-center gap-3 rounded-2xl px-4 py-2 transition-transform hover:ring-2 active:scale-95 hover:ring-white hover:backdrop-blur-sm hover:bg-gray-50/25"
                     target="_blank"
