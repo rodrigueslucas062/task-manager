c1a0e885a1f72ac2b97befbb538e264641ae70c5
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-04-02 23:51:14 -0300

    🎨💄 Layout optimizations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,42 +51,34 @@ const Navbar = () => {
                     </Dialog.Trigger>
 
                     <Dialog.Portal>
-                        <Dialog.DialogOverlay className="inset-0 fixed bg-black/20">
+                        <Dialog.DialogOverlay className="inset-0 fixed bg-black/70">
                             <Dialog.DialogContent className="fixed z-10 inset-0 md:inset-auto max-md:top-2/3 lg:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-gray-200 max-md:rounded-t-3xl lg:rounded-3xl flex flex-col outline-none overflow-hidden">
                                 <Dialog.Close className="hover:bg-gray-300 p-2 rounded-full absolute top-2 right-2 text-zinc-800">
                                     <X className="size-5" />
                                 </Dialog.Close>
-                                <div className="flex items-center justify-center gap-3 px-4 py-1.5">
-                                    <div className="rounded-lg mt-8 inline-block w-5/6 relative text-zinc-900">
+                                <div className="flex items-center justify-center gap-3 px-2 lg:px-4 py-1.5">
+                                    <div className="rounded-lg mt-4 lg:mt-8 inline-block w-5/6 relative text-zinc-900">
                                         <div className="flex flex-col items-center justify-center space-y-2">
                                             <span className="font-semibold text-zinc-900 text-lg">Lista diárias</span>
-                                            <div className="flex flex-col mt-6 space-y-4 font-semibold">
-                                                <div className="flex items-center justify-between px-3 rounded-md hover:bg-gray-300">
+                                            <div className="flex flex-col mt-4 lg:mt-6 space-y-4 font-semibold">
+                                                <button className="flex justify-between px-3 rounded-md hover:bg-gray-300 py-2 lg:py-2.5" onClick={handleCopyLink}>
                                                     <Share2 />
                                                     <span>Compartilhar esse projeto</span>
-                                                    <div className="hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full" onClick={handleCopyLink}>
-                                                        <ChevronRight />
-                                                    </div>
-                                                </div>
-                                                <div
-                                                    className="flex items-center justify-between px-3 rounded-md hover:bg-gray-300"
-                                                    onClick={handleToggleTheme}
-                                                >
+                                                    <ChevronRight />
+                                                </button>
+                                                <button className="flex justify-between px-3 rounded-md hover:bg-gray-300 py-2.5"
+                                                    onClick={handleToggleTheme} >
                                                     {isSun ? <Sun /> : <Moon />}
                                                     <span>Mudar tema</span>
-                                                    <div className="hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full">
-                                                        <ChevronRight />
-                                                    </div>
-                                                </div>
+                                                    <ChevronRight />
+                                                </button>
                                                 <div className="fixed flex flex-col space-y-2 bottom-0">
-                                                    <div className="flex items-center justify-between mb-2 px-3 rounded-md group hover:ring-2 hover:ring-red-600">
+                                                    <button className="flex items-center justify-between mb-2 px-3 rounded-md group hover:ring-2 hover:ring-red-600 py-2.5">
                                                         <Trash2 className="w-6 h-6 group-hover:text-red-600" />
                                                         <span className="flex-grow ml-4 group-hover:text-red-600">Remover todas as tarefas</span>
-                                                        <div className="hover:bg-zinc-300 hover:text-zinc-200 p-2 rounded-full group-hover:text-red-600">
-                                                            <ChevronRight className="w-6 h-6" />
-                                                        </div>
-                                                    </div>
-                                                    <div className="flex items-center justify-center pb-3">
+                                                        <ChevronRight className="group-hover:text-red-600"/>
+                                                    </button>
+                                                    <div className="flex justify-center pb-3">
                                                         <span className="text-sm text-zinc-900 font-semibold">Made for </span>
                                                         <FlaskConical className="text-lime-500" />{" "}
                                                         <span className="text-sm text-zinc-900 font-semibold">By Lucas</span>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,8 +5,7 @@ import { useState } from "react";
 import { toast } from "sonner";
 
 const TaskItem = ({ shadowStyle, index }) => {
-    const [taskText, setTaskText] = useLocalStorage(`taskText_${index}`, '')
-    const [completed, setCompleted] = useState(false)
+    const [taskText, setTaskText] = useLocalStorage(`taskText_${index}`, '');
 
     const handleChange = (e) => {
         const text = e.target.value;
@@ -14,10 +13,9 @@ const TaskItem = ({ shadowStyle, index }) => {
     };
 
     const handleDone = () => {
-        localStorage.removeItem(`taskText_${index}`)
-        setCompleted(true)
-        setTaskText('')
-        console.log(`taskText_${index}`)
+        localStorage.removeItem(`taskText_${index}`);
+        setTaskText('');
+        console.log(`taskText_${index}`);
         toast.success('Tarefa concluída!', {
             position: 'bottom-center',
             duration: 2000,
@@ -33,7 +31,7 @@ const TaskItem = ({ shadowStyle, index }) => {
                     </div>
                     <input
                         type="text"
-                        className="bg-transparent focus:outline-none w-40 lg:w-[420px] border-2 border-zinc-700"
+                        className="bg-transparent focus:outline-none w-40 lg:w-[420px]"
                         placeholder="Digite aqui..."
                         value={taskText}
                         onChange={handleChange}
@@ -56,19 +54,39 @@ const TaskItem = ({ shadowStyle, index }) => {
 const Tasks = () => {
     const shadowStyle = { boxShadow: "8px 8px 0px rgba(0, 0, 0, 0.75)" };
 
+    const [tasks, setTasks] = useState([...Array(5)].map((_, index) => ({
+        id: index,
+        text: localStorage.getItem(`taskText_${index}`) || ''
+    })));
+
+    const handleRemove = (index) => {
+        localStorage.removeItem(`taskText_${index}`);
+        const updatedTasks = tasks.filter(task => task.id !== index);
+        setTasks(updatedTasks);
+        console.log(`taskText_${index}`);
+        toast('Tarefa removida', {
+            action: {
+                label: 'Desfazer',
+                onClick: () => console.log('Undo')
+            },
+            position: 'bottom-center',
+            duration: 2000,
+        });
+    }
+
     return (
         <Dialog.Root>
             <section className="min-h-screen">
                 <div className="flex flex-col items-center pt-48 space-y-4 max-lg:px-4">
                     <h1 className="bg-sky-700 py-1 px-3 rounded-3xl font-semibold">Tarefas gerais</h1>
-                    {[...Array(5)].map((_, index) => (
-                        <TaskItem key={index} shadowStyle={shadowStyle} index={index} />
+                    {tasks.map(task => (
+                        <TaskItem key={task.id} shadowStyle={shadowStyle} index={task.id} handleRemove={handleRemove} />
                     ))}
                 </div>
             </section>
 
             <Dialog.Portal>
-                <Dialog.DialogOverlay className="inset-0 fixed bg-black/20">
+                <Dialog.DialogOverlay className="inset-0 fixed bg-black/70">
                     <Dialog.DialogContent className="fixed z-10 inset-0 md:inset-auto max-md:top-3/4 lg:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-gray-200 max-md:rounded-t-3xl lg:rounded-3xl flex flex-col outline-none overflow-hidden">
                         <Dialog.Close className="hover:bg-gray-300 p-2 rounded-full absolute top-2 right-2 text-zinc-900">
                             <X className="size-5" />
@@ -80,20 +98,17 @@ const Tasks = () => {
                                         Mais opções
                                     </span>
                                     <div className="flex flex-col mt-6 space-y-4 font-semibold">
-                                        <div className="flex items-center justify-between px-3 rounded-md border-2 border-zinc-700 hover:bg-gray-300">
-                                            <ListTodo className="w-6 h-6" />
+                                        <button className="flex justify-between px-3 rounded-md border-2 border-zinc-700 hover:bg-gray-300 py-2.5">
+                                            <ListTodo />
                                             <span className="flex-grow ml-4">Salvar nas tarefas diárias</span>
-                                            <div className="hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full">
-                                                <ChevronRight className="w-6 h-6" />
-                                            </div>
-                                        </div>
-                                        <div className="flex items-center justify-between px-3 rounded-md border-2 border-zinc-700 hover:bg-gray-300">
-                                            <Trash2 className="w-6 h-6" />
-                                            <span className="flex-grow ml-4">Remover da lista de tarefas</span>
-                                            <div className="hover:bg-zinc-400 hover:text-zinc-200 p-2 rounded-full">
-                                                <ChevronRight className="w-6 h-6" />
-                                            </div>
-                                        </div>
+                                            <ChevronRight />
+                                        </button>
+                                        <button className="flex items-center justify-between mb-2 px-3 rounded-md group hover:ring-2 hover:ring-red-600 py-2.5"
+                                            onClick={handleRemove}>
+                                            <Trash2 className="w-6 h-6 group-hover:text-red-600" />
+                                            <span className="flex-grow ml-4 group-hover:text-red-600">Remover todas as tarefas</span>
+                                            <ChevronRight className="group-hover:text-red-600" />
+                                        </button>
                                     </div>
                                 </div>
                             </div>
