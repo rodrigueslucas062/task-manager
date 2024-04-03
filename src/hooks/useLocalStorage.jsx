9ab7e40a58bcf2e4306df271bbe6d0e0a3f6c0d9
Lucas Rodrigues <39422637+rodrigueslucas062@users.noreply.github.com>
2024-04-03 11:15:08 -0300

    🐛 Client-side verification

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,31 +1,32 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue) {
-  const [storedValue, setStoredValue] = useState(initialValue)
+  const [storedValue, setStoredValue] = useState(initialValue);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (isClient) {
       try {
-        const item = localStorage.getItem(key)
-        setStoredValue(item ? JSON.parse(item) : initialValue)
+        const item = localStorage.getItem(key);
+        setStoredValue(item ? JSON.parse(item) : initialValue);
       } catch (error) {
-        console.warn(error)
-        setStoredValue(initialValue)
+        console.warn(error);
+        setStoredValue(initialValue);
       }
     }
-  }, [key, initialValue])
+  }, [isClient, initialValue, key]);
 
   const setValue = (value) => {
     try {
-      const ValueToStore = 
-      typeof value === "function" ? value(storedValue) : value
-      setStoredValue(ValueToStore)
-      if (typeof window !== "undefined") {
-        window.localStorage.setItem(key, JSON.stringify(ValueToStore))
+      const ValueToStore =
+        typeof value === "function" ? value(storedValue) : value;
+      setStoredValue(ValueToStore);
+      if (isClient) { // Apenas executa o código do lado do cliente
+        localStorage.setItem(key, JSON.stringify(ValueToStore));
       }
     } catch (error) {
-      console.warn(error)
+      console.warn(error);
     }
-  }
-  return [storedValue, setValue]
+  };
+
+  return [storedValue, setValue];
 }
