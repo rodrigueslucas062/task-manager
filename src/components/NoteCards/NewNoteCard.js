6cc9687b159d342b81ae5fa7a5c966119696a716
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-04-08 00:00:13 -0300

    🚀🚧 Notepad cards

---
+++ src/components/NoteCards/NewNoteCard.js
@@ -0,0 +1,151 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { DialogContent } from "@radix-ui/themes";
+import { X } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
+
+const NewNoteCards = ({ onNoteCreated }) => {
+  const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true);
+  const [content, setContent] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+
+  function handleStartEditor() {
+    setShouldShowOnBoarding(false);
+  }
+
+  function handleContentchanged(event) {
+    setContent(event.target.value);
+    if (event.target.value == "") {
+      setShouldShowOnBoarding(true);
+    }
+  }
+
+  function hanleSaveNote(event) {
+    event.preventDefault();
+    if (content === "") {
+      return;
+    }
+
+    onNoteCreated(content);
+    toast.success("Nota criada com sucesso");
+    setContent("");
+    setShouldShowOnBoarding(true);
+  }
+
+  function handleStartRecording(event) {
+    const isSpeechRecognitionAPIAvaliable =
+      "SpeechRecognition" in window || "webkitSpeechRecognition" in window;
+    if (!isSpeechRecognitionAPIAvaliable) {
+      alert("Seu navegador não duporta essa função");
+      return;
+    }
+
+    setIsRecording(true);
+    setShouldShowOnBoarding(false);
+
+    const SpeechRecognitionAPI =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    const speechRecognition = new SpeechRecognitionAPI();
+
+    speechRecognition.lang = "pt-BR";
+    speechRecognition.continuous = true;
+    speechRecognition.maxAlternatives = 1;
+    speechRecognition.interimResults = true;
+
+    speechRecognition.onresult = (event) => {
+      const transcription = Array.from(event.results).reduce((text, result) => {
+        return text.concat(result[0].transcript);
+      }, "");
+      setContent(transcription);
+    };
+    speechRecognition.onerror = (event) => {
+      console.error(event);
+    };
+    speechRecognition.start();
+  }
+
+  function handleStopRecording() {
+    setIsRecording(false);
+  }
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className="flex flex-col rounded-md text-left bg-slate-700 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600">
+        <span className="text-sm font-medium text-slate-200">
+          Adicionar nota
+        </span>
+        <p className="text-sm leading-6 text-slate-400 ">
+          Grave uma nota em áudio que será convertida para texto
+          automaticamente.
+        </p>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.DialogOverlay className="inset-0 fixed bg-black/90">
+          <DialogContent className="fixed z-10 inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none overflow-hidden">
+            <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+              <X className="size-5" />
+            </Dialog.Close>
+
+            <form className="flex-1 flex flex-col" action="">
+              <div className="flex flex-1 flex-col gap-3 p-5">
+                <span className="text-sm font-medium text-slate-200">
+                  Adicionar nota
+                </span>
+
+                {shouldShowOnBoarding ? (
+                  <p className="text-sm leading-6 text-slate-400">
+                    Comece{" "}
+                    <button
+                      type="button"
+                      onClick={handleStartRecording}
+                      className="font-medium text-lime-400 hover:underline"
+                    >
+                      gravando uma nota em áudio
+                    </button>{" "}
+                    ou se preferir{" "}
+                    <button
+                      type="button"
+                      onClick={handleStartEditor}
+                      className="font-medium text-lime-400 hover:underline"
+                    >
+                      utilize apenas texto
+                    </button>{" "}
+                    automaticamente.
+                  </p>
+                ) : (
+                  <textarea
+                    autoFocus
+                    className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                    onChange={handleContentchanged}
+                    value={content}
+                  />
+                )}
+              </div>
+
+              {isRecording ? (
+                <button
+                  type="button"
+                  onClick={handleStopRecording}
+                  className="w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100"
+                >
+                  <div className="size-3 rounded-full bg-red-500 animate-pulse" />
+                  Gravando... (clique para parar)
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  onClick={hanleSaveNote}
+                  className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+                >
+                  Salvar nota?
+                </button>
+              )}
+            </form>
+          </DialogContent>
+        </Dialog.DialogOverlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
+export default NewNoteCards;
