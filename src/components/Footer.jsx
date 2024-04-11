756dab5aee930baf694f181942fe30ef808e8915
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2024-04-11 09:21:12 -0300

    🤡 Hidden footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Github } from "lucide-react"
 
 const Footer = () => {
     return (
-        <footer className="fixed bottom-0 h-8 w-full border-t-2 md:p-3 md:h-10 lg:hidden">
+        <footer className="fixed bottom-0 h-8 w-full border-t-2 md:p-3 md:h-10 hidden">
             <div className="flex items-center justify-around gap-4">
                 <a className="flex select-none items-center gap-3 rounded-2xl px-4 py-2 transition-transform hover:ring-2 active:scale-95 hover:ring-white hover:backdrop-blur-sm hover:bg-gray-50/25"
                     target="_blank"
