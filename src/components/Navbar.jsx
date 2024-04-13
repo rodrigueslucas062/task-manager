072e2af890129408f2c87ffeb99e0cfe202cd9f1
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-04-12 23:50:00 -0300

    ♻️📱 Refactoring and adjusting responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,47 +48,47 @@ const Navbar = () => {
                                 <Dialog.Close className="hover:bg-gray-300 p-2 rounded-full absolute top-2 right-2 text-zinc-800">
                                     <X className="size-5" />
                                 </Dialog.Close>
-                                <div className="flex items-center justify-center gap-3 px-2 lg:px-4 py-1.5">
-                                    <div className="rounded-lg mt-4 lg:mt-8 inline-block w-5/6 relative text-zinc-900">
-                                        <div className="flex flex-col items-center justify-center space-y-2">
+                                <div className="flex flex-col items-center justify-center gap-3 px-2 lg:px-4 pt-1.5">
+                                    <div className="rounded-lg mt-4 lg:mt-8 justify-center inline-block w-3/4 lg:w-3/5 relative text-zinc-900">
+                                        <div className='flex justify-center'>
                                             <span className="font-semibold text-zinc-900 text-lg">{notebookText}</span>
-                                            <div className="flex flex-col mt-4 lg:mt-6 space-y-4 lg:space-y-1.5 font-semibold">
-                                                <button className="flex justify-between px-3 rounded-md hover:bg-gray-300 py-2 lg:py-2.5" onClick={handleCopyLink}>
-                                                    <Share2 />
-                                                    <span>Compartilhar lista</span>
-                                                    <ChevronRight />
-                                                </button>
-                                                <Link href={notebookLink} className="flex justify-between px-3 rounded-md hover:bg-gray-300 py-2 lg:py-2.5">
-                                                    {notebookIcon}
-                                                    <span>{notepadText}</span>
-                                                    <ChevronRight />
-                                                </Link>
-                                                <Link href={'/dailytasks'}  className="flex justify-between px-3 rounded-md hover:bg-gray-300 py-2.5"
-                                                    onClick={handleToggleTheme} >
-                                                    <Sun />
-                                                    <span>Tarefas diárias</span>
-                                                    <ChevronRight />
-                                                </Link>
-                                                <button className="flex justify-between px-3 rounded-md hover:bg-gray-300 py-2.5"
-                                                    onClick={handleToggleTheme} >
-                                                    <Wallpaper />
-                                                    <span>Mudar tema</span>
-                                                    <ChevronRight />
-                                                </button>
-                                                <div className="fixed flex flex-col space-y-2 bottom-0">
-                                                    <button className="flex items-center justify-between mb-2 px-3 rounded-md group hover:ring-2 hover:ring-red-600 py-2.5">
-                                                        <Trash2 className="w-6 h-6 group-hover:text-red-600" />
-                                                        <span className="flex-grow ml-4 group-hover:text-red-600">Remover todas as tarefas</span>
-                                                        <ChevronRight className="group-hover:text-red-600" />
-                                                    </button>
-                                                    <div className="flex justify-center pb-3">
-                                                        <span className="text-sm text-zinc-900 font-semibold">Made for </span>
-                                                        <FlaskConical className="text-lime-500" />{" "}
-                                                        <span className="text-sm text-zinc-900 font-semibold">By Lucas</span>
-                                                    </div>
-                                                </div>
-                                            </div>
                                         </div>
+                                        <div className="flex flex-col mt- space-y-4 lg:space-y-1.5 font-semibold">
+                                            <button className="flex justify-between px-3 rounded-md hover:bg-gray-300 py-2 lg:py-3" onClick={handleCopyLink}>
+                                                <Share2 />
+                                                <span>Compartilhar lista</span>
+                                                <ChevronRight />
+                                            </button>
+                                            <Link href={notebookLink} className="flex justify-between px-3 rounded-md hover:bg-gray-300 py-2 lg:py-2.5">
+                                                {notebookIcon}
+                                                <span>{notepadText}</span>
+                                                <ChevronRight />
+                                            </Link>
+                                            <Link href={'/dailytasks'} className="flex justify-between px-3 rounded-md hover:bg-gray-300 py-2.5"
+                                                onClick={handleToggleTheme} >
+                                                <Sun />
+                                                <span>Tarefas diárias</span>
+                                                <ChevronRight />
+                                            </Link>
+                                            <button className="flex justify-between px-3 rounded-md hover:bg-gray-300 py-2.5"
+                                                onClick={handleToggleTheme} >
+                                                <Wallpaper />
+                                                <span>Mudar tema</span>
+                                                <ChevronRight />
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex text-zinc-800 font-semibold mt-auto pb-2 w-3/4 lg:w-3/5 mx-auto flex-col space-y-2 bottom-0">
+                                    <button className="flex items-center justify-between mb-2 px-3 rounded-md group hover:ring-2 hover:ring-red-600 py-2.5">
+                                        <Trash2 className="group-hover:text-red-600" />
+                                        <span className="flex-grow ml-4 group-hover:text-red-600">Remover todas as tarefas</span>
+                                        <ChevronRight className="group-hover:text-red-600" />
+                                    </button>
+                                    <div className="flex justify-center">
+                                        <span className="text-sm">Made for </span>
+                                        <FlaskConical className="text-lime-500" />{" "}
+                                        <span className="text-sm">By Lucas</span>
                                     </div>
                                 </div>
                             </Dialog.DialogContent>
