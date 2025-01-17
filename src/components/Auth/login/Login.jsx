632a455453bb6190439aa07ea4202bf3accc1d95
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-01-17 07:58:10 -0300

    ✨: Substitui elementos <a> por <Link> nas páginas de login e registro para melhor integração com o Next.js

--- src/components/Auth/login/Login.jsx
+++ src/components/Auth/login/Login.jsx
@@ -92,7 +92,7 @@ export default function Login() {
                                     Sign in
                                 </button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <a href="/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                                    Don’t have an account yet? <Link href="/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
                                 </p>
                             </form>
                         </div>
--- src/components/Auth/register/Register.jsx
+++ src/components/Auth/register/Register.jsx
@@ -67,7 +67,7 @@ export default function Register() {
                                 Create Account
                             </button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Already have an account? <a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign in</a>
+                                Already have an account? <Link href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign in</Link>
                             </p>
                         </form>
                     </div>
