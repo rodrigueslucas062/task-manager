93fa1074782398dd657a65e80d4cff7e77ef03de
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-01-20 20:20:55 -0300

    ✨: Ajusta posicionamento de elementos na tela de login e cadastro

--- src/components/Auth/register/SingUp.jsx
+++ src/components/Auth/register/SingUp.jsx
@@ -30,9 +30,9 @@ export default function Signup() {
     return (
         <section className="w-full h-screen flex justify-center items-center bg-gradient-to-r from-zinc-950 to-zinc-800">
             <div className="relative">
-                <div className="absolute top-[-50px] right-[-50px] w-24 h-24 bg-purple-400 bg-opacity-10 backdrop-blur-sm shadow-lg border border-white border-opacity-20 rounded-lg animated bounce-slow"></div>
+                <div className="absolute top-[-50px] right-[-30px] lg:right-[-50px] w-24 h-24 bg-purple-400 bg-opacity-10 backdrop-blur-sm shadow-lg border border-white border-opacity-20 rounded-lg animated bounce-slow"></div>
                 <div className="absolute top-[150px] left-[-100px] w-[120px] h-[200px] bg-sky-600 bg-opacity-10 backdrop-blur-sm shadow-lg border border-white border-opacity-20 rounded-lg animated bounce-fast"></div>
-                <div className="absolute bottom-[-80px] right-[-50px] w-[70px] h-[70px] bg-sky-600 bg-opacity-10 backdrop-blur-sm shadow-lg border border-white border-opacity-20 rounded-lg animated bounce-medium"></div>
+                <div className="absolute bottom-[-80px] right-[-30px] lg:right-[-50px] w-[70px] h-[70px] bg-sky-600 bg-opacity-10 backdrop-blur-sm shadow-lg border border-white border-opacity-20 rounded-lg animated bounce-medium"></div>
                 <div className="absolute bottom-[-60px] w-[50px] h-[50px] bg-purple-400 bg-opacity-10 backdrop-blur-sm shadow-lg border border-white border-opacity-20 rounded-lg rotate-[10deg] animated bounce-slow"></div>
                 <div className="absolute top-[-60px] left-[150px] w-[60px] h-[60px] bg-zinc-700 bg-opacity-10 backdrop-blur-sm shadow-lg border border-white border-opacity-20 rounded-lg animated bounce-medium"></div>
 
