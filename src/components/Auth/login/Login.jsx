1b8132b347e2f52c1b21bcbf84538bfbcc79d3cb
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-01-21 23:55:13 -0300

    ✨: Atualiza o gradiente de fundo do AuthLayout e ajusta o posicionamento dos elementos no layout de login

--- src/components/Auth/login/Login.jsx
+++ src/components/Auth/login/Login.jsx
@@ -41,66 +41,64 @@ export default function Login() {
       <div>
          {isAuthenticated && router.push("/tasks")}
          <AuthLayout>
-            <div className="relative w-full lg:w-[500px] min-h-[500px] flex justify-center items-center bg-white bg-opacity-10 backdrop-blur-sm shadow-lg border border-white border-opacity-50 rounded-lg">
-               <div className="relative w-full h-full p-10">
-                  <div className="space-y-2">
-                     <h2 className="text-white text-2xl font-semibold tracking-wider">
-                        Synapse
-                     </h2>
-                     <div className="left-0 bottom-[-10px] w-28 h-1 bg-purple-400"></div>
+            <div className="relative w-full h-full p-10">
+               <div className="space-y-2">
+                  <h2 className="text-white text-2xl font-semibold tracking-wider">
+                     Synapse
+                  </h2>
+                  <div className="left-0 bottom-[-10px] w-28 h-1 bg-purple-400"></div>
+               </div>
+               <form action="#" onSubmit={handleSubmit}>
+                  <div className="w-full mt-5 relative">
+                     <span className="absolute inset-y-0 left-3 flex items-center text-white">
+                        <Envelope size={20} weight="duotone" />
+                     </span>
+                     <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        className="w-full bg-white bg-opacity-20 px-10 py-2.5 text-white text-[15px] tracking-[1px] rounded-lg border border-white border-opacity-50 shadow-[0_5px_15px_rgba(0,0,0,0.05)] focus:outline-none"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        required
+                     />
                   </div>
-                  <form action="#" onSubmit={handleSubmit}>
-                     <div className="w-full mt-5 relative">
-                        <span className="absolute inset-y-0 left-3 flex items-center text-white">
-                           <Envelope size={20} weight="duotone" />
-                        </span>
-                        <input
-                           type="email"
-                           name="email"
-                           id="email"
-                           placeholder="Email"
-                           className="w-full bg-white bg-opacity-20 px-10 py-2.5 text-white text-[15px] tracking-[1px] rounded-lg border border-white border-opacity-50 shadow-[0_5px_15px_rgba(0,0,0,0.05)] focus:outline-none"
-                           value={email}
-                           onChange={(event) => setEmail(event.target.value)}
-                           required
-                        />
-                     </div>
-                     <div className="w-full mt-5 relative">
-                        <span className="absolute inset-y-0 left-3 flex items-center text-white">
-                           <Lock size={20} weight="duotone" />
-                        </span>
-                        <input
-                           type={showPassword ? "text" : "password"}
-                           name="password"
-                           id="password"
-                           placeholder="Senha"
-                           className="w-full bg-white bg-opacity-20 px-10 py-2.5 text-white text-[15px] tracking-[1px] rounded-lg border border-white border-opacity-50 shadow-[0_5px_15px_rgba(0,0,0,0.05)] focus:outline-none"
-                           value={password}
-                           onChange={(event) => setPassword(event.target.value)}
-                           required
-                        />
-                        <span
-                           onClick={togglePasswordVisibility}
-                           className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-white"
-                        >
-                           {showPassword ? <EyeSlash size={20} weight="duotone" /> : <Eye size={20} weight="duotone" />}
-                        </span>
-                     </div>
-                     <div className="mt-10">
-                        <button type="submit" className="flex items-center justify-center gap-3 w-full px-5 py-2 rounded-lg font-semibold bg-white text-black mb-5" >
-                           Login
-                           <SignIn size={24} weight="duotone" />
-                        </button>
-                     </div>
-                     <p className="text-white mt-sm">
-                        Esqueceu a senha? <Link href="#" className="font-semibold text-white">Clique aqui</Link>
-                     </p>
-                  </form>
-                  <div className="mt-20">
-                     <p className="text-white mt-sm">
-                        <Link href="/singup" className="font-semibold text-white">Sign up</Link>
-                     </p>
+                  <div className="w-full mt-5 relative">
+                     <span className="absolute inset-y-0 left-3 flex items-center text-white">
+                        <Lock size={20} weight="duotone" />
+                     </span>
+                     <input
+                        type={showPassword ? "text" : "password"}
+                        name="password"
+                        id="password"
+                        placeholder="Senha"
+                        className="w-full bg-white bg-opacity-20 px-10 py-2.5 text-white text-[15px] tracking-[1px] rounded-lg border border-white border-opacity-50 shadow-[0_5px_15px_rgba(0,0,0,0.05)] focus:outline-none"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        required
+                     />
+                     <span
+                        onClick={togglePasswordVisibility}
+                        className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-white"
+                     >
+                        {showPassword ? <EyeSlash size={20} weight="duotone" /> : <Eye size={20} weight="duotone" />}
+                     </span>
                   </div>
+                  <div className="mt-10">
+                     <button type="submit" className="flex items-center justify-center gap-3 w-full px-5 py-2 rounded-lg font-semibold bg-white text-black mb-5" >
+                        Login
+                        <SignIn size={24} weight="duotone" />
+                     </button>
+                  </div>
+                  <p className="text-white mt-sm">
+                     Esqueceu a senha? <Link href="#" className="font-semibold text-white">Clique aqui</Link>
+                  </p>
+               </form>
+               <div className="mt-20">
+                  <p className="text-white mt-sm">
+                     <Link href="/singup" className="font-semibold text-white">Sign up</Link>
+                  </p>
                </div>
             </div>
          </AuthLayout>
--- src/components/Auth/register/SingUp.jsx
+++ src/components/Auth/register/SingUp.jsx
@@ -14,7 +14,7 @@ export default function Signup() {
     const [showPassword, setShowPassword] = useState(false);
 
     const togglePasswordVisibility = () => {
-       setShowPassword(!showPassword);
+        setShowPassword(!showPassword);
     };
 
     const handleSubmit = async (event) => {
@@ -45,43 +45,65 @@ export default function Signup() {
 
     return (
         <AuthLayout>
-            <div className="relative w-full lg:w-[500px] min-h-[500px] flex justify-center items-center bg-white bg-opacity-10 backdrop-blur-sm shadow-lg border border-white border-opacity-50 rounded-lg">
-                <div className="relative w-full h-full p-10">
-                    <div className="space-y-2">
-                        <h2 className="text-white text-2xl font-semibold tracking-wider">
-                            Synapse
-                        </h2>
-                        <div className="left-0 bottom-[-10px] w-28 h-1 bg-purple-400"></div>
+            <div className="relative w-full h-full p-10">
+                <div className="space-y-2">
+                    <h2 className="text-white text-2xl font-semibold tracking-wider">
+                        Synapse
+                    </h2>
+                    <div className="left-0 bottom-[-10px] w-28 h-1 bg-purple-400"></div>
+                </div>
+                <form action="#" onSubmit={handleSubmit}>
+                    <div className="w-full mt-5 relative">
+                        <span className="absolute inset-y-0 left-3 flex items-center text-white">
+                            <Envelope size={20} weight="duotone" />
+                        </span>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Email"
+                            className="w-full bg-white bg-opacity-20 px-10 py-2.5 text-white text-[15px] tracking-[1px] rounded-lg border border-white border-opacity-50 shadow-[0_5px_15px_rgba(0,0,0,0.05)] focus:outline-none"
+                            value={email}
+                            onChange={(event) => setEmail(event.target.value)}
+                            required
+                        />
                     </div>
-                    <form action="#" onSubmit={handleSubmit}>
-                        <div className="w-full mt-5 relative">
-                            <span className="absolute inset-y-0 left-3 flex items-center text-white">
-                                <Envelope size={20} weight="duotone" />
-                            </span>
-                            <input
-                                type="email"
-                                name="email"
-                                id="email"
-                                placeholder="Email"
-                                className="w-full bg-white bg-opacity-20 px-10 py-2.5 text-white text-[15px] tracking-[1px] rounded-lg border border-white border-opacity-50 shadow-[0_5px_15px_rgba(0,0,0,0.05)] focus:outline-none"
-                                value={email}
-                                onChange={(event) => setEmail(event.target.value)}
-                                required
-                            />
-                        </div>
 
+                    <div className="w-full mt-5 relative">
+                        <span className="absolute inset-y-0 left-3 flex items-center text-white">
+                            <Lock size={20} weight="duotone" />
+                        </span>
+                        <input
+                            type={showPassword ? "text" : "password"}
+                            name="password"
+                            id="password"
+                            placeholder="Senha"
+                            className="w-full bg-white bg-opacity-20 px-10 py-2.5 text-white text-[15px] tracking-[1px] rounded-lg border border-white border-opacity-50 shadow-[0_5px_15px_rgba(0,0,0,0.05)] focus:outline-none"
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                            required
+                        />
+                        <span
+                            onClick={togglePasswordVisibility}
+                            className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-white"
+                        >
+                            {showPassword ? <EyeSlash size={20} weight="duotone" /> : <Eye size={20} weight="duotone" />}
+                        </span>
+                    </div>
+
+                    {passwordConfirmation !== undefined && (
                         <div className="w-full mt-5 relative">
                             <span className="absolute inset-y-0 left-3 flex items-center text-white">
                                 <Lock size={20} weight="duotone" />
                             </span>
                             <input
                                 type={showPassword ? "text" : "password"}
-                                name="password"
-                                id="password"
-                                placeholder="Senha"
+                                name="passwordConfirmation"
+                                id="passwordConfirmation"
+                                placeholder="Confirmar Senha"
                                 className="w-full bg-white bg-opacity-20 px-10 py-2.5 text-white text-[15px] tracking-[1px] rounded-lg border border-white border-opacity-50 shadow-[0_5px_15px_rgba(0,0,0,0.05)] focus:outline-none"
-                                value={password}
-                                onChange={(event) => setPassword(event.target.value)}
+                                value={passwordConfirmation}
+                                onChange={(event) => setPasswordConfirmation(event.target.value)}
                                 required
                             />
                             <span
@@ -91,50 +113,26 @@ export default function Signup() {
                                 {showPassword ? <EyeSlash size={20} weight="duotone" /> : <Eye size={20} weight="duotone" />}
                             </span>
                         </div>
+                    )}
 
-                        {passwordConfirmation !== undefined && (
-                            <div className="w-full mt-5 relative">
-                                <span className="absolute inset-y-0 left-3 flex items-center text-white">
-                                    <Lock size={20} weight="duotone" />
-                                </span>
-                                <input
-                                    type={showPassword ? "text" : "password"}
-                                    name="passwordConfirmation"
-                                    id="passwordConfirmation"
-                                    placeholder="Confirmar Senha"
-                                    className="w-full bg-white bg-opacity-20 px-10 py-2.5 text-white text-[15px] tracking-[1px] rounded-lg border border-white border-opacity-50 shadow-[0_5px_15px_rgba(0,0,0,0.05)] focus:outline-none"
-                                    value={passwordConfirmation}
-                                    onChange={(event) => setPasswordConfirmation(event.target.value)}
-                                    required
-                                />
-                                <span
-                                    onClick={togglePasswordVisibility}
-                                    className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-white"
-                                >
-                                    {showPassword ? <EyeSlash size={20} weight="duotone" /> : <Eye size={20} weight="duotone" />}
-                                </span>
-                            </div>
-                        )}
-
-                        {error && (
-                            <div className="text-red-500 text-sm mt-2">
-                                {error}
-                            </div>
-                        )}
-
-                        <div className="mt-10">
-                            <button
-                                type="submit"
-                                className="flex items-center justify-center gap-3 w-full px-5 py-2 rounded-lg font-semibold bg-white text-black mb-5"
-                            >
-                                Cadastrar
-                            </button>
+                    {error && (
+                        <div className="text-red-500 text-sm mt-2">
+                            {error}
                         </div>
-                    </form>
-                    <p className="text-white mt-sm">
-                        <Link href="/" className="font-semibold text-white">Já tenho cadastro</Link>
-                    </p>
-                </div>
+                    )}
+
+                    <div className="mt-10">
+                        <button
+                            type="submit"
+                            className="flex items-center justify-center gap-3 w-full px-5 py-2 rounded-lg font-semibold bg-white text-black mb-5"
+                        >
+                            Cadastrar
+                        </button>
+                    </div>
+                </form>
+                <p className="text-white mt-sm">
+                    <Link href="/" className="font-semibold text-white">Já tenho cadastro</Link>
+                </p>
             </div>
         </AuthLayout>
     );
