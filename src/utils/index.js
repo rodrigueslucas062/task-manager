768456fae5a63054dbef2ae74b20e402abd5248a
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-02-15 01:39:37 -0300

    ♻️: Adiciona exportações nomeadas para novos componentes e funções utilitárias, melhorando a organização do código

---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+export { handleUpdateText } from "./utils";
+export { handleAddCircleNode } from "./utils";
+export { addSquareNode } from "./utils";
+export { addCircleNode } from "./utils";
+export { handleAddPostIt } from "./utils";
+export { handleAddNode } from "./utils";
+export { handleAddStickyNotesNode } from "./utils";
+export { handleDeleteNode } from "./utils";
