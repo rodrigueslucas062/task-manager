1167086425fad252098d42a7a3cf09d460142ea9
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-02-15 01:10:39 -0300

    ♻️: Altera o componente Daily para exportação nomeada, melhorando a estrutura do código

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,9 @@
 import Tasks from "./ToDo";
 
-const Daily = () => {
+export const Daily = () => {
   return (
     <div className="min-h-screen min-w-screen relative">
       <Tasks />
     </div>
   )
 }
-
-export default Daily;
