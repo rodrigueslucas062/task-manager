f187456ce93a3f1342d6b9af64e2c5205f46235c
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2025-02-17 00:28:22 -0300

    ✨: Adiciona funcionalidade de logout e personalização ao DialogModal, integrando ícone e informações do autor

--- src/components/DialogModal.jsx
+++ src/components/DialogModal.jsx
@@ -1,11 +1,14 @@
-import { DotsThreeOutline, XCircle } from "phosphor-react"
+import { Flask, XCircle } from "phosphor-react"
 import * as Dialog from "@radix-ui/react-dialog";
+import { useAuth } from "./Context/authContext/authContext";
 
-export const DialogModal = ({ children }) => {
+export const DialogModal = ({ children, Icon }) => {
+  const { logout } = useAuth();
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className="visible bg-zinc-200 hover:bg-zinc-400 text-zinc-900 hover:text-zinc-200 p-2 rounded-full">
-        <DotsThreeOutline size={20} weight="duotone" />
+        {Icon}
       </Dialog.Trigger>
 
       <Dialog.Portal>
@@ -15,6 +18,15 @@ export const DialogModal = ({ children }) => {
               <XCircle size={20} weight="duotone" />
             </Dialog.Close>
             {children}
+            <div className="flex text-zinc-800 font-semibold mt-auto pb-2 w-3/4 lg:w-3/5 mx-auto flex-col space-y-2 bottom-0">
+              <button className="flex justify-center items-center w-full py-1.5 bg-lime-500 text-white rounded-lg font-semibold"
+                onClick={() => logout()}>Logout</button>
+              <div className="flex justify-center">
+                <span className="text-sm">Made for </span>
+                <Flask size={20} weight="duotone" className="text-lime-500" />{" "}
+                <span className="text-sm">By Lucas</span>
+              </div>
+            </div>
           </Dialog.Content>
         </Dialog.Overlay>
       </Dialog.Portal>
